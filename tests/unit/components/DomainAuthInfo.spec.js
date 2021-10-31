b1b91105e5ce968be0e8c9e85718574d668d672e
Jakub Vala <jamm3e3333@gmail.com>
2021-10-31 16:34:57 +0100

    Add test for auth info

--- tests/unit/components/DomainAuthInfo.spec.js
+++ tests/unit/components/DomainAuthInfo.spec.js
@@ -1,13 +1,22 @@
 import DomainAuthInfo from '@/components/AuthInfo/DomainAuthInfo';
+import App from '@/App';
+
 import { mount } from '@vue/test-utils';
+import data from '@/assets/domain-detail.json';
 
 describe('AuthInfo component', () => {
     test('Should mount the component', () => {
-        mount(DomainAuthInfo, {
+        const userInfo = mount(DomainAuthInfo, {
             props: {
-                expires: '10-02-2021',
-                show: false,
+                expires: data.expires_at,
+                show: true,
             }
         });
+
+        expect(userInfo.find('p.expires-value').text())
+        .toBe(new Date(data.expires_at).toISOString());
+
+        expect(userInfo.find('button.btn-toggle')
+        .text()).toBe('SHOW');
     })
 });
